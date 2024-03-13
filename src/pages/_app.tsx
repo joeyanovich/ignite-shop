c07c3a8d190ab91d9db8b64cecffd90424c9aa11
joeyanovich <joedison.dias@gmail.com>
2024-03-13 14:52:54 -0300

    feat: click on the logo to direct to home

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Container, Header } from "@/styles/pages/app";
 import Image from 'next/image'
 
 import logoImg from '../assets/logo.svg'
+import Link from "next/link";
 
 globalStyles()
 
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <Image src={logoImg} alt="" />
+        <Link href="/">
+          <Image src={logoImg} alt="" />
+        </Link>
       </Header>
       <Component {...pageProps} />
     </Container>
