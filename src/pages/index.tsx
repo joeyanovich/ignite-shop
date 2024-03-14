6803bc6288b99b95f9d174e6caa0a72dd362ece7
joeyanovich <joedison.dias@gmail.com>
2024-03-13 23:46:21 -0300

    refactor: add nullability check in getStaticProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,7 +30,7 @@ export default function Home({ products }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Ignite Shop</title>
+        <title>Home | Ignite Shop</title>
       </Head>
 
       <HomeContainer ref={sliderRef} className="keen-slider" >
--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -81,7 +81,13 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({ params }) => {
-  const productId = params.id;
+  const productId = params?.id;
+
+  if (!productId){
+    return {
+      notFound: true,
+    }
+  }
 
   const product = await stripe.products.retrieve(productId, {
     expand: ['default_price']
